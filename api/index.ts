b4bc81c6238c366f616c88af7670e42430d96783
Apoena <machadoapoena@gmail.com>
2026-07-02 08:52:09 -0300

    ajuste

---
+++ api/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import router from "../api-src/routes";
+
+const app = express();
+
+// Body parsing middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Mount all API routes under /api
+app.use("/api", router);
+
+export default app;
